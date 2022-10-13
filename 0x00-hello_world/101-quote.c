7fada15bfa015ca6cc261400ee6bfb45f4c6e253
Easter21 <fasika.hailu21@gmail.com>
2022-10-13 13:58:55 -0700

    don't worry

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - display "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * on new line
+ *
+ * Return: value 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 64);
+	return (1);
+}
+
+
