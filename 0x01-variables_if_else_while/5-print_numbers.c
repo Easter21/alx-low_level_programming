ca3a322df0af08b4d8585076ee1d737c9bd7d160
Easter21 <fasika.hailu21@gmail.com>
2022-10-16 10:43:53 -0700

    c conditions

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	for (num ; num < 10 ; num++)
+	{
+		printf("%i", num);
+		printf("\n");
+	}
+}
