6243a9fd3e13280023b62a667073ac55c35a4a36
Easter21 <fasika.hailu21@gmail.com>
2022-10-18 06:27:24 -0700

    c function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_abs - print absolute value of integer
+ *@a: absolute value input  parameter
+ *Return: integer value
+ */
+
+int _abs(int a)
+{
+	if (a > 0)
+		return (a);
+	else if (a < 0)
+		return (-a);
+
+	return (0);
+}
