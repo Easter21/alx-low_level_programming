a29a90478594ec7d62c83fec44cd73065af33ded
Easter21 <fasika.hailu21@gmail.com>
2022-10-18 04:36:08 -0700

    c function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print all lowercase alphabet 10 times
+ *
+ *Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	/* char lowerch = 'a';*/
+	int counter = 1;
+
+	while (counter <= 10)
+	{
+		char lowerch = 'a';
+		while (lowerch <= 'z')
+		{
+			_putchar(lowerch);
+			lowerch++;
+		}
+		counter++;
+		_putchar('\n');
+	}
+}
