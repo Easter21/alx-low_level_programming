2a30c85f736b14c0f84d3d45cd6e1e38d2c32351
Easter21 <fasika.hailu21@gmail.com>
2022-10-18 05:05:53 -0700

    c function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_islower - check either lower character or other
+ *@c: checking character
+ *Return: int type 0 or 1
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
