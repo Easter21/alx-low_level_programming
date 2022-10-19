9e250f4dd19a15ccb52a0595bf6c9fa5b4685873
Easter21 <fasika.hailu21@gmail.com>
2022-10-19 11:36:06 -0700

    c function

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,22 +8,23 @@
  */
 int main(void)
 {
-	unsigned long int t0 = 0, t1 = 1, s;
+	long int t0 = 0, t1 = 1, s;
 	int i;
 
-	for (i = 1; i < 98; i++)
+	for (i = 1; i <= 98; i++)
 	{
 		s = t0 + t1;
-		if (i != 98)
+		if (i == 98)
 		{
-			printf("%lu, ", s);
+			printf("%lu", s);
 		}
 		else
 		{
-			printf("%lu\n", s);
+			printf("%lu, ", s);
 		}
 		t0 = t1;
 		t1 = s;
 	}
+	printf("\n");
 	return (0);
 }
