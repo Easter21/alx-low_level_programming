168fcba660ff77df6269f8a80d880be6014efe2f
Easter21 <fasika.hailu21@gmail.com>
2022-10-25 09:33:24 -0700

    string and pointer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *put2 - print every other character
+ *@str: string pointer
+ *Return: void
+ */
+
+void puts2(char *str)
+{
+	int j = 0;
+	
+	while (str[j] != '\0')
+	{
+		if ((j % 2)==0)
+
+			_putchar(str[j]);
+		/* else */
+			/* continue; */
+		j++;
+	}
+	_putchar('\n');
+}
