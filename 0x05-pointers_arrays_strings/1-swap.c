e640a111e2012da7f10cf5c786fe7c084f91441c
Easter21 <fasika.hailu21@gmail.com>
2022-10-25 06:30:04 -0700

    string and pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *swap_int - swap the values of the two pointer variable each other
+ *@*a: first pointer parameter for function
+ *@*b: second pointer parameter
+ *Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	int t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+void swap_int(int *a, int *b);
+
+#endif
