55fb1204f736cb388fa96bc24c21d617603e39e1
Easter21 <fasika.hailu21@gmail.com>
2022-10-25 11:39:29 -0700

    string and pointer

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,16 +8,20 @@
 
 void puts_half(char *str)
 {
-	int k = 0;
+	int k = 0, i, l;
 
-	for (; str[k] != '\0' ; k++)
+	for (int i = 0; str[i] != '\0'; i++)
+		k++;
+	l = (k / 2);
+	if ((k % 2) != 0)
 	{
-		if ((k % 2) != 0)
-		{
-			_putchar(str[k]);
-		}
-		/* k++; */
+		l = ((k + 1) / 2);
 	}
+
+	for (i = l; str[i] != '\0'; i++)
+		_putchar(str[i]);
+		/* k++; */
+
 	_putchar('\n');
 
 }
