9f07ef39e143299aeec8eeede1c3552f71761c22
Easter21 <fasika.hailu21@gmail.com>
2022-10-27 04:58:12 -0700

    string and pointer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ **_strcat - appends the src string to dest string
+ *@dest: argument of _strcat function
+ *@src: argument of _strcat function
+ *Return: string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *result = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+	return (result);
+}
