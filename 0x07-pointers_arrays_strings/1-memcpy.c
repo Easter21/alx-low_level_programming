e9c5892025f75376110bc0c2f37e22d61d473725
Easter21 <fasika.hailu21@gmail.com>
2022-11-01 13:09:30 -0700

    string and pointer

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ **_memcpy - copy memory area
+ *@dest: pointer to charcter variable as destination
+ *@src: pointer to character varable as source
+ *@n: number of bytes
+ *Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int size = n;
+
+	if (size > 0)
+	{
+		int k;
+
+		k = 0;
+		while (k < size)
+		{
+			dest[k] = src[k];
+			k++;
+		}
+	}
+
+	return (dest);
+}
