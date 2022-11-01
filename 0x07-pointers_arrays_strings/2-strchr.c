7fe906b14324ea27ad5de2de7856d7db049a14d7
Easter21 <fasika.hailu21@gmail.com>
2022-11-01 15:26:56 -0700

    string and pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ **_strchr - locates chracter instring
+ *@s: pointer to character
+ *@c:character type variable
+ *Return: the first occurance of charcter
+ */
+
+char *_strchr(char *s, char c)
+{
+	int k;
+
+	k = 0;
+	while (s[k] >= '\0')
+	{
+		if (s[k] == c)
+			return (s + 1);
+		k++;
+	}
+
+	return (NULL);
+}
