7606ed5f890d197d3a619b4901b5dd46ed8dfa17
Easter21 <fasika.hailu21@gmail.com>
2022-11-02 12:35:04 -0700

    string and pointer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
 /**
  * is_prime - recursively divide by higher divisor, skip even nums
  * @n: number to check if prime
- * @divisor: divisor
+ * @di: divisor
  * Return: 1 if prime, 0 if not, or recursive function call
  */
 
