875336c5a4949c55e47a32d04da035cbecdce663
Easter21 <fasika.hailu21@gmail.com>
2022-11-06 23:42:55 -0800

    argc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	}
 	/* int mul = atoi(argv[1]) * atoi(argv[2]); */
 
-	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 
 	return (0);
