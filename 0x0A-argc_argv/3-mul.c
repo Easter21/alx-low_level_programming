0d61913331a00fe16a469e61c632cb0c34bbd30f
Easter21 <fasika.hailu21@gmail.com>
2022-11-06 23:35:30 -0800

    argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - multiples od the two numbers
+ *@argc: counter argument
+ *@argv: pointer of array argument
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Errot\n");
+		return (1);
+	}
+	int mul = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%i\n", mul);
+
+
+	return (0);
+}
